fc469460cc8a395f08f216f659bd9efafb9e169b
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-09-29 10:20:46 +0200

    Set correct cookie domain when running locally

--- setup/global-setup.ts
+++ setup/global-setup.ts
@@ -1,5 +1,6 @@
-import { chromium } from '@playwright/test';
+import { Page, chromium } from '@playwright/test';
 import { FullConfig } from '@playwright/test/reporter';
+import { USE_DEV } from '../tests/functions';
 import { getLoggedInPage } from '../tests/helpers';
 import { userSaksbehandler } from '../tests/users';
 
@@ -10,6 +11,10 @@ const globalSetup = async (config: FullConfig) => {
   await getLoggedInPage(page, userSaksbehandler);
 
   if (typeof storageState === 'string') {
+    if (!USE_DEV) {
+      await setLocalhostCookie(page);
+    }
+
     await page.context().storageState({ path: storageState });
   }
 
@@ -17,3 +22,19 @@ const globalSetup = async (config: FullConfig) => {
 };
 
 export default globalSetup;
+
+const setLocalhostCookie = async (page: Page) => {
+  const cookies = await page.context().cookies('https://kabal.dev.nav.no');
+
+  if (!Array.isArray(cookies) || cookies.length === 0) {
+    throw new Error('Did not find any cookies for kabal.dev.nav.no');
+  }
+
+  if (cookies.length > 1) {
+    throw new Error('Found more than one cookie for kabal.dev.nav.no');
+  }
+
+  await page.context().clearCookies();
+
+  await page.context().addCookies([{ ...cookies[0], domain: 'localhost' }]);
+};
