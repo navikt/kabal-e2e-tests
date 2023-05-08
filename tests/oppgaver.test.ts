f72113aa697c3d239806c2a40e4c3e34619d0cdf
Christian Skrøvseth <christian.skrovseth@nav.no>
2023-05-08 17:14:30 +0200

    Remove page number in URL

--- tests/oppgaver.test.ts
+++ tests/oppgaver.test.ts
@@ -3,12 +3,12 @@ import { UI_DOMAIN, getParsedUrl } from './functions';
 
 test.describe('Oppgaver', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto(`${UI_DOMAIN}/oppgaver/1`);
+    await page.goto(`${UI_DOMAIN}/oppgaver`);
   });
 
   test('"Oppgaver"-siden vises', async ({ page }) => {
     const url = getParsedUrl(page.url());
-    expect(url.pathname).toBe('/oppgaver/1');
+    expect(url.pathname).toBe('/oppgaver');
     await page.waitForSelector('data-testid=oppgave-table-rows');
   });
 });
