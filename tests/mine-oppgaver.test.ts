3b501de41f92263faa684aa3aad53e64cdc9de89
Christian Skrøvseth <christian.skrovseth@nav.no>
2023-05-09 13:43:03 +0200

    Fix Mine Oppgaver test steps

--- tests/mine-oppgaver.test.ts
+++ tests/mine-oppgaver.test.ts
@@ -1,26 +1,25 @@
 import { expect, test } from '@playwright/test';
 import { UI_DOMAIN, getParsedUrl } from './functions';
 
+const TABLES: [string, string][] = [
+  ['Tildelte oppgaver', 'mine-oppgaver-table-rows'],
+  ['Oppgaver på vent', 'oppgaver-paa-vent-table-rows'],
+  ['Fullførte oppgaver', 'fullfoerte-oppgaver-table-rows'],
+];
+
 test.describe('Mine Oppgaver', () => {
-  test.beforeEach(async ({ page }) => {
+  test(`Alle (${TABLES.length}) oppgavetabeller laster og viser oppgaver`, async ({ page }) => {
     await page.goto(`${UI_DOMAIN}/mineoppgaver`);
-  });
-
-  const tables: [string, string][] = [
-    ['Mine Oppgaver', 'mine-oppgaver-table-rows'],
-    ['Oppgaver på vent', 'oppgaver-paa-vent-table-rows'],
-    ['Fullførte oppgaver', 'fullfoerte-oppgaver-table-rows'],
-  ];
-
-  test('"Mine Oppgaver" laster og viser behandlinger', async ({ page }) => {
     const url = getParsedUrl(page.url());
 
     expect(url.pathname).toBe('/mineoppgaver');
 
-    for (const [tableName, tableId] of tables) {
-      test.step(tableName, async () => {
-        await page.waitForSelector(`[data-testid="${tableId}"][data-state="ready"]`);
-      });
-    }
+    await Promise.all(
+      TABLES.map(([table, tableId]) =>
+        test.step(table, async () => {
+          await page.waitForSelector(`[data-testid="${tableId}"][data-state="ready"]`);
+        })
+      )
+    );
   });
 });
