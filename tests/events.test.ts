1bba6a2b31e87a572094d7c8b6f5b1a2325a24e1
Christian Skrøvseth <christian.skrovseth@gmail.com>
2023-12-28 16:12:27 +0100

    Events stress test

---
+++ tests/events.test.ts
@@ -0,0 +1,197 @@
+import { fail } from 'assert';
+import { Page, expect, test } from '@playwright/test';
+import EventSource from 'eventsource';
+import { UI_DOMAIN } from './functions';
+
+const NUMBER_OF_CONNECTIONS = 400;
+const MESSAGE_TEXT = 'Hello world!';
+const BEHANDLING_ID = '563b9561-a46d-4aa2-95df-a187aad10834';
+
+test.describe('Events', () => {
+  test.only(`Alle (${NUMBER_OF_CONNECTIONS}) SSE subscribers får events`, async ({ page }) => {
+    console.log(`Connecting ${NUMBER_OF_CONNECTIONS} SSE clients...`);
+    const connectStart = performance.now();
+
+    const sseClients = await connectSSE(page, `${UI_DOMAIN}/api/kabal-api/behandlinger/${BEHANDLING_ID}/events`);
+
+    console.log(`Connected ${NUMBER_OF_CONNECTIONS} SSE clients in ${time(connectStart)}.`);
+
+    console.log(`Waiting for 10 seconds...`);
+    await page.waitForTimeout(10_000);
+    console.log(`Waited for 10 seconds.`);
+
+    sendMessage(page);
+
+    await test.step(`Wait for ${NUMBER_OF_CONNECTIONS} SSE messages`, async () => {
+      console.log(`Waiting for ${NUMBER_OF_CONNECTIONS} SSE messages...`);
+      const start = performance.now();
+
+      try {
+        await waitForSSE(sseClients);
+        console.log(`Received ${NUMBER_OF_CONNECTIONS} messages in ${time(start)}.`);
+      } catch (e) {
+        if (e instanceof Error) {
+          fail(e);
+        } else {
+          fail(new Error(`Failed to receive ${NUMBER_OF_CONNECTIONS} messages in ${time(start)}.`));
+        }
+      } finally {
+        await closeSSE(sseClients);
+      }
+    });
+  });
+});
+
+const sendMessage = async (page: Page): Promise<void> => {
+  console.log(`Sending message...`);
+  const start = performance.now();
+
+  const res = await fetch(`${UI_DOMAIN}/api/kabal-api/behandlinger/${BEHANDLING_ID}/meldinger`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Cookie: (await page.context().cookies()).map(({ name, value }) => `${name}=${value}`).join('; '),
+    },
+    body: JSON.stringify({ text: MESSAGE_TEXT }),
+  });
+
+  expect(res.status).toBe(201);
+
+  console.log(`Message sent successfully in ${time(start)}`);
+};
+
+const connectSSE = async (page: Page, url: string): Promise<EventSource[]> => {
+  const headers: Record<string, string> = {
+    Accept: 'text/event-stream',
+    Connection: 'keep-alive',
+    Cookie: (await page.context().cookies()).map(({ name, value }) => `${name}=${value}`).join('; '),
+  };
+
+  const sseOptions: EventSource.EventSourceInitDict = {
+    rejectUnauthorized: true,
+    withCredentials: true,
+    headers,
+  };
+
+  const sseClients: EventSource[] = [];
+
+  return new Promise<EventSource[]>((resolve, reject) => {
+    for (let i = 0; i < NUMBER_OF_CONNECTIONS; i++) {
+      setTimeout(() => {
+        const start = performance.now();
+
+        try {
+          const sse = new EventSource(`${url}?id=${i}`, sseOptions);
+
+          sse.addEventListener('open', () => {
+            console.log(`SSE client ${i + 1} connected in ${time(start)}.`);
+
+            if (sseClients.push(sse) === NUMBER_OF_CONNECTIONS) {
+              resolve(sseClients);
+            }
+          });
+
+          sse.addEventListener('error', (event) => {
+            console.error(`SSE client ${i + 1} error after ${time(start)}`, event);
+            reject(event);
+          });
+        } catch (e) {
+          console.error(`Failed to connect SSE client ${i + 1} after ${time(start)}`, e);
+          reject(e);
+        }
+      }, 100 * i);
+    }
+  }).catch((e) => {
+    closeSSE(sseClients);
+    throw e;
+  });
+};
+
+const SSE_TIMEOUT = 15_000;
+
+const waitForSSE = async (sseClients: EventSource[]): Promise<void> => {
+  let received = 0;
+
+  return new Promise<void>((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      for (const sse of sseClients) {
+        sse.close();
+      }
+
+      reject(
+        new Error(`Timed out waiting for SSE after ${SSE_TIMEOUT} ms. Got ${received} of ${NUMBER_OF_CONNECTIONS}`),
+      );
+    }, SSE_TIMEOUT);
+
+    for (const sse of sseClients) {
+      const start = performance.now();
+      sse.addEventListener('MESSAGE_ADDED', (event) => {
+        console.log(`SSE client ${received + 1} received message in ${time(start)}.`);
+
+        if (isServerSentEvent(event)) {
+          const data = parseJSON<IMessage>(event.data);
+
+          if (data?.text === MESSAGE_TEXT) {
+            received += 1;
+
+            if (received === NUMBER_OF_CONNECTIONS) {
+              clearTimeout(timeout);
+              resolve();
+            }
+          }
+        }
+      });
+    }
+  });
+};
+
+const closeSSE = async (sseClients: EventSource[]): Promise<void> => {
+  const startClose = performance.now();
+  console.log(`Closing ${sseClients.length} SSE clients...`);
+
+  for (const sse of sseClients) {
+    sse.close();
+  }
+
+  console.log(`Closed ${sseClients.length} SSE clients in ${time(startClose)}.`);
+};
+
+const time = (start: number): string => {
+  const ms = performance.now() - start;
+
+  if (ms < 1000) {
+    return `${ms.toFixed(2)} ms`;
+  }
+
+  if (ms < 60_000) {
+    return `${(ms / 1000).toFixed(2)} s`;
+  }
+
+  return `${Math.floor(ms / 60_000)} min, ${(ms % 60_000).toFixed(2)} s`;
+};
+
+interface IMessage {
+  author: {
+    name: string;
+    saksbehandlerIdent: string;
+  };
+  created: string;
+  id: string;
+  modified: string;
+  text: string;
+}
+
+type ServerSentEvent = MessageEvent<string>;
+
+const isServerSentEvent = (event: Event): event is ServerSentEvent =>
+  'data' in event && typeof event.data === 'string' && 'lastEventId' in event && typeof event.lastEventId === 'string';
+
+const parseJSON = <T>(json: string): T | null => {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    console.warn('Failed to parse JSON', json, e);
+
+    return null;
+  }
+};
