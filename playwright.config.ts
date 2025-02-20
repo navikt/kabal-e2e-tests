a74471ab70bdc76080cdbcb9fa8d502cf200cd52
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-02-20 08:40:07 +0100

    Speed up tests by using default (small) viewport and split configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,28 +1,17 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { SCREEN_SIZE } from './setup/global-setup';
-
-const config: PlaywrightTestConfig = {
-  outputDir: '/tmp/test-results',
-  workers: 4,
-  fullyParallel: true,
-  timeout: 600_000,
-  globalTimeout: 600_000,
-  name: 'KABAL',
-  reporter: [['list'], ['./reporters/slack-reporter.ts']],
-  retries: 1,
-  testDir: './tests',
-  use: {
-    actionTimeout: 15_000,
-    video: { mode: 'on', size: SCREEN_SIZE },
-    screenshot: 'on',
-    trace: 'on',
-    locale: 'no-NB',
-    viewport: SCREEN_SIZE,
-    storageState: '/tmp/state.json', // File for storing cookies and localStorage (per origin). Speeds up test execution, as the test browser no longer needs to log in for every test.
-  },
-  // https://playwright.dev/docs/test-advanced#global-setup-and-teardown
-  globalSetup: require.resolve('./setup/global-setup'),
-};
+import { defineConfig } from '@playwright/test';
+import { baseConfig } from './playwright.config.base';
 
 // biome-ignore lint/style/noDefaultExport: https://playwright.dev/docs/test-configuration
-export default config;
+export default defineConfig({
+  ...baseConfig,
+  outputDir: '/tmp/test-results',
+  reporter: [['list'], ['./reporters/slack-reporter.ts']],
+  retries: 1,
+
+  use: {
+    ...baseConfig.use,
+    video: 'on',
+    screenshot: 'on',
+    storageState: '/tmp/state.json', // File for storing cookies and localStorage (per origin). Speeds up test execution, as the test browser no longer needs to log in for every test.
+  },
+});
