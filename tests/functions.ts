20c1c971ffb5e5f297dfe77df3a6080800e2275e
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-05-09 10:48:41 +0200

    test

--- tests/functions.ts
+++ tests/functions.ts
@@ -7,7 +7,7 @@ export const getParsedUrl = (url: string): URL => new URL(url);
 
 export const USE_LOCAL = process.env.TEST_ENV === 'local';
 
-export const DEV_DOMAIN = 'https://kabal.intern.dev.nav.no';
+export const DEV_DOMAIN = 'http://kabal-frontend';
 export const LOCAL_DOMAIN = 'http://localhost:8061';
 
 export const UI_DOMAIN = USE_LOCAL ? LOCAL_DOMAIN : DEV_DOMAIN;
